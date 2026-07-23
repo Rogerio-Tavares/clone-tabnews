2d6234cb5f4e4396e7f3961455c7be58cea1be95
Rogerio Tavares <oliveirarogerio585@gmail.com>
2026-07-23 00:59:40 -0300

    Adicionando pasta pages

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+//Define uma função para renderizar a página.
+function Home(){
+    return <h1>Hello World! It's OK!</h1>
+}
+
+//Exportar essa função pra fora do arquivo.
+export default Home;
+//Deve-se usar o 'defalt' para definir como padrão.
+//Tornando-se o Índice de páginas.
